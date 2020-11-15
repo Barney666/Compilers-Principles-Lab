a89d8fd3819a6352307a43afe14c1134833a51d4
Barney <540747702@qq.com>
2020-11-15 21:34:54 +0800

    Lab1 词法分析（main.cpp调lex.yy.c函数）

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -7,28 +7,25 @@ using namespace std;
 extern "C" int lex(const char* input);     // 返回值表示是否有词法错误
 
 int main(int argc, char* argv[]) {
-    char *a = "0xFF32";
-    char *ptr;
-    printf("%d",strtol(a, &ptr, 16));
-//    ifstream file(argv[1]);
-//    string temp;
-//    string file_content = "";
-//    if (!file.is_open()) {
-//        cout << "未成功打开文件" << endl;
-//    }
-//    while (getline(file, temp)) {
-//        file_content.append(temp);
-//        file_content = file_content + '\n';
-//    }
-//    file.close();
-//    if(!lex(file_content.data())){
-//        ifstream out_file("src/out.txt");
-//        cerr << "";    // 没有这个下面没法输出，好离谱啊，这玩意要事先激活的？
-//        if(out_file.is_open()){
-//            while (getline(out_file, temp))
-//                cerr << temp << endl;
-//        }
-//        out_file.close();
-//    }
-//    return 0;
+    ifstream file(argv[1]);
+    string temp;
+    string file_content = "";
+    if (!file.is_open()) {
+        cout << "未成功打开文件" << endl;
+    }
+    while (getline(file, temp)) {
+        file_content.append(temp);
+        file_content = file_content + '\n';
+    }
+    file.close();
+    if(!lex(file_content.data())){
+        ifstream out_file("src/out.txt");
+        cerr << "";    // 没有这个下面没法输出，好离谱啊，这玩意要事先激活的？
+        if(out_file.is_open()){
+            while (getline(out_file, temp))
+                cerr << temp << endl;
+        }
+        out_file.close();
+    }
+    return 0;
 }
